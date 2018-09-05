5408cf39734f3b551fd1d12d397be7770f32c44a
Niko Heeren <nheeren@ethz.ch>
2018-09-05 17:11:15 -0400

    New function for comprehensive aspects from metadata.
    - Some changes to README (update TODO list, formatting)
    - New function get_aspects_table() to get a comprehensive table for
       the aspects from the template file metadata
    - adapted / simplified valid_attribute() to work with the new function

--- IEDC_tools/validate.py
+++ IEDC_tools/validate.py
@@ -1,6 +1,7 @@
 """
 Functions to validate the input files prior to database insert / upload.
 """
+import pandas as pd
 
 from IEDC_tools import dbio, inputs
 
@@ -9,7 +10,7 @@ def valid_aspect_class(file):
     """
     Checks if all aspects that are specified in the data template file are actually defined in the database (aspects
     table).
-    :param file: The candidate data filename
+    :param file: Candidate data file name. String.
     :return: None. Does only assert.
     """
     meta = inputs.read_candidate_meta(file)
@@ -25,32 +26,55 @@ def valid_attribute(file):
     :return:
     """
     file_data = inputs.read_candidate_data(file)
-    meta_dict = inputs.read_candidate_meta(file)
-    classifications = meta_dict['classifications'].to_dict()['Aspects_Attribute_No']
-    meta = meta_dict['meta']
-    # Filter only the columns that start with 'aspect' from the dataset information metadata table
-    file_aspects = meta[meta.index.str.startswith('aspect')]
-    for aspect, attrib_name in file_aspects.to_dict()['Dataset entries'].items():
-        if aspect.endswith("classification"):
-            continue
+    aspect_table = get_aspects_table(file)
+    aspect_table = aspect_table[aspect_table['classification_id'] != 'custom']
+    for aspect in aspect_table.index:
         # Get the number of the aspect from dataset information metadata table
         aspect_no = aspect.split('_')[-1]
         # This is the classification id value for the 'aspect_x_classification' key in dataset information.
         # It will be used to look up the values from the classification_items table.
-        classification_id = meta.loc["aspect_%s_classification" % aspect_no, 'Dataset entries']
-        if classification_id not in ['custom', 'none']:
-            # Get the attribute number to select the right column (e.g. attribute2) from the classification_items table
-            attrib_no = classifications[attrib_name]
-            # Get all possible values from the classification_items table.
-            # For more performance this could be taken out of the loop and filtered in a dataframe.
-            db_attributes = dbio.get_sql_as_df('classification_items',
-                                               addSQL="WHERE classification_id = %s" % classification_id)
-            # Select the candidates from above's query
-            checkme = db_attributes["attribute" + str(attrib_no)].values
-            # Now go through all unique items in the database file and see if it is contained in
-            #   the classification_items table.
-            for attribute in file_data[attrib_name].unique():
-                assert str(attribute) in checkme, "'%s' not in %s" % (attribute, checkme)
+        classification_id = aspect_table.loc[aspect, 'classification_id']
+        # Get the attribute number to select the right column (e.g. attribute2) from the classification_items table
+        attrib_no = aspect_table.loc[aspect, 'attribute_no']
+        attrib_name = aspect_table.loc[aspect, 'name']
+        # Get all possible values from the classification_items table.
+        # For better performance this could be taken out of the loop and looked up from a dataframe.
+        db_attributes = dbio.get_sql_as_df('classification_items',
+                                           addSQL="WHERE classification_id = %s" % classification_id)
+        # Select the candidates from above's query
+        checkme = db_attributes["attribute" + str(attrib_no)].values
+        # Now go through all unique items in the database file and see if it is contained in
+        #   the classification_items table.
+        for attribute in file_data[attrib_name].unique():
+            assert str(attribute) in checkme, "'%s' not in %s" % (attribute, checkme)
+
+
+def get_aspects_table(file):
+    """
+        Pulls the info on classification and attributes together, i.e. make sense of the messy attributes in an actual
+        table... More of a convenience function for tired programmers.
+        See sheet 'Cover' in template file.
+        :param file: Filename, string
+        :return: Dataframe table with name, classification_id, and attribute_no
+        """
+    # Read the file and put metadata and classifications in two variables
+    file_meta = inputs.read_candidate_meta(file)
+    meta = file_meta['meta']
+    classifications = file_meta['classifications']
+    # Filter relevant rows from the metadata table, i.e. the ones containing 'aspect'
+    custom_aspects = meta[meta.index.str.startswith('aspect_')]
+    custom_aspects = custom_aspects[custom_aspects.index.str.endswith('_classification')]
+    # Get rid of the empty ones
+    custom_aspects = custom_aspects[custom_aspects['Dataset entries'] != 'none']
+    # Here comes the fun... Let's put everything into a dict, because that is easily converted to a dataframe
+    d = {'classification_id': custom_aspects['Dataset entries'].values,
+         'index': [i.replace('_classification', '') for i in custom_aspects.index],
+         'name': meta.loc[[i.replace('_classification', '') for i in custom_aspects.index]]['Dataset entries'].values,
+         }
+    d['attribute_no'] = classifications.loc[d['name']]['Aspects_Attribute_No'].values
+    # Convert to df and get rid of the redundant 'index' column
+    aspect_table = pd.DataFrame(d, index=d['index']).drop('index', axis=1)
+    return aspect_table
 
 
 # TODO add custom classifications
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Industrial Ecology Data Commons Tools
 
-**Under early development. May later be merged into [Industrial Ecology Data Commons project](https://github.com/IndEcol/IE_data_commons).**
+***Under early development. May later be merged into [Industrial Ecology Data Commons project](https://github.com/IndEcol/IE_data_commons).***
 
 A collection of tools to interact with the [Industrial Ecology Data Commons project](https://github.com/IndEcol/IE_data_commons). It pareses data input files, validates and uploades them into the database.
 
@@ -23,11 +23,11 @@ Credits: Niko Heeren, Stefan Pauliuk
 
 ## TODO
 
-- [ ] Create repo
-- [ ] Add routine for creating a custom classification
+- [ ] Routine for creating a custom classification
 - [ ] Routine for data upload
 
-
+- [x] Convenience function to pull classification attributes together
+- [x] Create repo
 - [x] Validation function to check if all aspects exist in the database
 - [x] Validation function to check if attributes are present in the DB
 - [x] Write basic IO functions
